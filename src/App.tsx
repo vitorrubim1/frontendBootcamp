107c11c9ccffca7cf31f0cbf04fa89004584faab
Vitor Rubim <vitorrubim1@hotmail.com>
2021-01-27 18:35:20 -0300

    feat: realizando a validaçao do login, com a mudança do formulario comum, para o do unform, e validaçao de campo com yup

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 import GlobalStyles from "./styles/global";
 
-// import SignIn from "./SignIn";
-import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+// import SignUp from "./SignUp";
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyles />
-      {/* <SignIn /> */}
-      <SignUp />
+      <SignIn />
+      {/* <SignUp /> */}
     </>
   );
 };
--- src/SignIn/index.tsx
+++ src/SignIn/index.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useRef, useCallback } from "react";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import * as Yup from "yup";
+import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core"; // typagem da referencia do formulario
+
+import getValidationErrors from "../utils/getValidationsErrors";
 
 import LogoImg from "../assets/logo.svg";
 
@@ -8,34 +13,60 @@ import Button from "../components/Button";
 
 import { Container, Content, Background } from "./styles";
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={LogoImg} alt="Logo GoBarber" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <form>
-        <h1>Faça seu login</h1>
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({}); // zerando os erros
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
-        <Button type="submit">Entrar</Button>
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("Email obrigatório")
+          .email("Digite um email válido"),
+        password: Yup.string().required("Senha obrigatória"),
+      });
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+      await schema.validate(data, {
+        abortEarly: false, // pra retornar todos os erros de uma vez
+      }); // dados q recebi do input
+    } catch (error) {
+      const errors = getValidationErrors(error);
 
-      <a href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
+      formRef.current?.setErrors(errors); // formRef: referencia do formulario. current: valor das informações
+    }
+  }, []);
 
-    <Background />
-  </Container>
-);
+  return (
+    <Container>
+      <Content>
+        <img src={LogoImg} alt="Logo GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu login</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
