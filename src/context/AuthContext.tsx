ce5f8f00176e6319a5a83f8df548096b32bcc509
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-03-20 20:53:48 -0300

    chore: mantendo no localStorage as credenciais de autenticaçao

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,9 +1,9 @@
-import React, { createContext, useCallback } from "react";
+import React, { createContext, useCallback, useState } from "react";
 
 import { api } from "../services/api";
 
 interface AuthContextData {
-  name: string;
+  user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
@@ -12,19 +12,41 @@ interface SignInCredentials {
   password: string;
 }
 
+interface AuthState {
+  token: string;
+  user: object; // não defino exatamente, pq a api pode mudar os dados de retorno
+}
+
 export const AuthContext = createContext<AuthContextData>(
   {} as AuthContextData
 );
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem("@GoBarber:token");
+    const user = localStorage.getItem("@GoBarber:user");
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState; // se não achar nada no localStorage, retorna um objeto vazio
+  }); // isso é vai funcionar quando der refresh na página, ou quando voltar página
+
+  // função de login/autenticação
   const signIn = useCallback(async ({ email, password }: SignInCredentials) => {
     const response = await api.post("sessions", { email, password });
 
-    console.log(response.data);
+    const { userData: user, token } = response.data; // desacoplando informações que vem da api
+
+    localStorage.setItem("@GoBarber:token", token);
+    localStorage.setItem("@GoBarber:user", JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: "Vitor", signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
